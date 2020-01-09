72f070d1aefc9410369ef2515959d2f0284d251c
Ivan Elv <purenaturalism@gmail.com>
2020-01-09 16:26:03 +0700

    Create main system class and setup initial config.

---
+++ src/nachos/proj1/MainSystem.java
@@ -0,0 +1,15 @@
+package nachos.proj1;
+
+import nachos.proj1.utilities.Console;
+
+public class MainSystem
+{
+	private Console console;
+
+	public MainSystem()
+	{
+		console = Console.getInstance();
+
+		console.println("Something");
+	}
+}
