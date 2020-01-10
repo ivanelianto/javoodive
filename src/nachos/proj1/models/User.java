00d1f61d043dd34b2565d83f310653a60af8fb96
Ivan Elv <purenaturalism@gmail.com>
2020-01-10 11:01:54 +0700

    Create id generator.

--- src/nachos/proj1/models/User.java
+++ src/nachos/proj1/models/User.java
@@ -1,12 +1,41 @@
 package nachos.proj1.models;
 
+import java.util.Random;
+
 public class User
 {
+	private static final int ID_LENGTH = 33;
+	private String id;
 	private String username;
+	private int balance;
+
+	public User()
+	{
+		this.id = generateID();
+	}
 
 	public User(String username)
 	{
+		this();
 		this.username = username;
+		this.balance = 0;
+	}
+
+	public User(String username, int balance)
+	{
+		this();
+		this.username = username;
+		this.balance = balance;
+	}
+
+	public String getId()
+	{
+		return id;
+	}
+
+	public void setId(String id)
+	{
+		this.id = id;
 	}
 
 	public String getUsername()
@@ -17,5 +46,57 @@ public class User
 	public void setUsername(String username)
 	{
 		this.username = username;
+	}
+
+	public int getBalance()
+	{
+		return balance;
+	}
+
+	public void setBalance(int balance)
+	{
+		this.balance = balance;
+	}
+
+	private String generateID()
+	{
+		StringBuilder sb = new StringBuilder();
+
+		Random random = new Random();
+
+		while (sb.length() < ID_LENGTH)
+		{
+			int charCode = random.nextInt('z');
+			
+			if (isAcceptableCharCode(charCode))
+				sb.append((char) charCode);
+		}
+		
+		String result = sb.toString();
+		
+		return result.substring(0, 1).toUpperCase() +
+				result.substring(1);
+	}
+
+	private boolean isAcceptableCharCode(int charCode)
+	{
+		return isDigit(charCode)
+			|| isUppercaseLetter(charCode)
+			|| isLowercaseLetter(charCode);
+	}
+
+	private boolean isLowercaseLetter(int charCode)
+	{
+		return charCode >= 'a' && charCode <= 'z';
+	}
+
+	private boolean isUppercaseLetter(int charCode)
+	{
+		return charCode >= 'A' && charCode <= 'Z';
+	}
+
+	private boolean isDigit(int charCode)
+	{
+		return charCode >= '0' && charCode <= '9';
 	}
 }
